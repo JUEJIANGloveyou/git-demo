a5221b08a3b5c2fe51b914fd6ba9d76a855f269a
Sakura-Q <1067458966@qq.com>
2018-06-12 23:41:44 +0800

    tree

---
+++ maketree.py
@@ -0,0 +1,29 @@
+from turtle import *
+def tree(plist, l, a, f):
+    if l>5:
+        lst=[]
+        for p in plist:
+           p.forward(1)
+           q=p.clone()
+           p.left(a)
+           q.right(a)
+           lst.append(p)
+           lst.append(q)
+        tree(lst, l*f, a, f)
+def maketree(x, y):
+    p=Turtle()
+    p.color("green")
+    p.pensize(5)
+    p.hideturtle()
+    p.getscreen().tracer(30, 0)
+    p.left(90)
+    p.penup()
+    p.goto(x, y)
+    p.pendown()
+    tree([p], 110, 65, 0.6375)
+    print(len(p.getscreen().turtles()))
+def main():
+    maketree(-200, 200)
+    maketree(0, 0)
+    maketree(200, 200)
+main()
